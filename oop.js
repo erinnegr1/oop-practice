faa1c489e0091794a6c635bb4170acd52047869b
Erinne Grant <erinne.grant@gmail.com>
2022-08-29 13:50:07 -0700

    alien

--- oop.js
+++ oop.js
@@ -63,21 +63,49 @@ function shipTest2(HP, speed){
 class Shiptest3 {
   constructor(HP, speed) {
     this.HP = HP;
-    this speed = speed;
+    this.speed = speed;
   }
 
-  firebullet(param1, param2){ // ship.firebullet(param1, param2) 
+  firebullet(param1, param2){ 
     return bulletTest(param1, param2)
-    
   }
+  // call it like so:
+  // ship.firebullet(param1, param2) 
 }
 
 
 /////Alien
 
 //Factory Function
+
+function alien(HP, coords, directs){
+  /*HP is numeric, coordinates is an object of two values: X and Y, speeds is an object of two values: X and Y
+
+  coordinates will look like this {x:122, y:2333}
+  */
+
+  return {
+    HP: HP;
+    coordinates: coords, //{x,y}
+    direction: directs,
+    move: function(change_x, change_y) {
+      this.coordinates.x = this.coordinates.x + change_x;
+      this.coordinates.y = this.coordinates.y + change_y;
+    },
+    onHit: function(bullet){
+      this.HP = this.HP - bullet.damage;
+    }  
+  }
+}
+
+//call it like this:
+//alien_person = alien (1000, {x:123, y:222}, {x_speed: 5, y_speed: 10})
+
 // Constructor Function
 // Class Syntax
+
+
+
 
 ///// Barrier
 
