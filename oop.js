781ab03db2803219bcd70692955dc63425b0397a
Erinne Grant <erinne.grant@gmail.com>
2022-08-29 14:40:05 -0700

    alienexamples

--- oop.js
+++ oop.js
@@ -76,19 +76,21 @@ class Shiptest3 {
 
 /////Alien
 
+
 //Factory Function
 
 function alien(HP, coords, directs){
   /*HP is numeric, coordinates is an object of two values: X and Y, speeds is an object of two values: X and Y
-
+  * Move updates direction based on speed
+  * Onhit(bullet) deals damage to the alien
   coordinates will look like this {x:122, y:2333}
   */
 
   return {
-    HP: HP;
+    HP, // any time the key is the same as variable, you can use "HP," for syntactic sugar
     coordinates: coords, //{x,y}
     direction: directs,
-    move: function(change_x, change_y) {
+    move: function(change_x, change_y) {  // a method is a function that is defined inside of class
       this.coordinates.x = this.coordinates.x + change_x;
       this.coordinates.y = this.coordinates.y + change_y;
     },
@@ -97,17 +99,81 @@ function alien(HP, coords, directs){
     }  
   }
 }
+const = alien_person = alien (1000, {x:123, y:222}, {x_speed: 5, y_speed: 10});
 
-//call it like this:
-//alien_person = alien (1000, {x:123, y:222}, {x_speed: 5, y_speed: 10})
+/*call it like this:
+alien_person = alien (1000, {x:123, y:222}, {x_speed: 5, y_speed: 10})
+console.log(alien)
+alien.move();
+console.log(alien)
+*/
 
-// Constructor Function
-// Class Syntax
+function alien(HP, coordinates, direction){
+  /*Same as above, but written with syntactic sugar
+  */
 
+  return {
+    HP, // any time the key is the same as variable, you can use "HP," for syntactic sugar
+    coordinates, //{x,y}
+    direction,
+    move: function(change_x, change_y) {  // a method is a function that is defined inside of class
+      this.coordinates.x = this.coordinates.x + change_x; // using this is more explicit and makes sure we are refering to coordinates defined in the param
+      this.coordinates.y = this.coordinates.y + change_y;
+      // coordinates.x += direction.x;
+      // coordinates.y += direction.y;
+    },
+    onHit: function(bullet){
+      this.HP = this.HP - bullet.damage;
+    }  
+  }
+}
+
+////// Constructor Function
+
+function AlienConstructor(HP, coordinates, direction){
+
+    this.hp = hp;
+    this.coordinates = coordinates;
+    this.direction = direction;
+    this.move = function () {  
+      this.coordinates.x += this.direction.x; 
+      this.coordinates.y += this.direction.y;
+    };
+    this onHit = function(bullet){
+      this.HP -= bullet.damage;
+    }  
+  }
+
+///// Class Syntax
+
+class AlienClass {
+  constructor(hp, coords, directions) {
+    this.hp = hp;
+    this.speeds = speeds;
+  }
+
+  move() {
+    this.coords.x += this.direction.x;
+    this.coords.y += this.speeds.y;
+  }
+
+  onHit(bullet) {
+    this.hp -= bullet.damage;
+  }
+}
 
 
 
 ///// Barrier
+
+/**
+ * 
+ * 
+ * 
+ *  
+ * 
+ */
+
 
 //Factory Function
 // Constructor Function
